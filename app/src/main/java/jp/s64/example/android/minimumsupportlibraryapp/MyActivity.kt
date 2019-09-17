a9d56509d916eb990fa5790efb5f20444964ca96
Shuma Yoshioka <shuma_yoshioka@voyagegroup.com>
2019-09-17 16:42:18 +0900

    Update to use support library

--- app/src/main/java/jp/s64/example/android/minimumsupportlibraryapp/MyActivity.kt
+++ app/src/main/java/jp/s64/example/android/minimumsupportlibraryapp/MyActivity.kt
@@ -1,9 +1,9 @@
 package jp.s64.example.android.minimumsupportlibraryapp
 
-import android.app.Activity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
-class MyActivity : Activity() {
+class MyActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
